968b344fcc064595d09f855597861df24a5796d4
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2026-06-21 23:14:30 -0300

    continue editing my atreus keymap later tomorrow ;)

--- keyboards/keyboardio/atreus/keymaps/mpbsd/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/mpbsd/keymap.c
@@ -1,38 +1,24 @@
 #include QMK_KEYBOARD_H
 
-enum layer_names {
-    _QW,
-    _RS,
-    _LW,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
+  [0] = LAYOUT(
+      LT(4,KC_Q), LT(3,KC_W), LT(2,KC_E), LT(1,KC_R),    KC_T,                     KC_Y, LT(1,KC_U), LT(2,KC_I), LT(3,KC_O), LT(4,KC_P),
+      LCTL(KC_A), LGUI(KC_S), LALT(KC_D), LSFT(KC_F),    KC_G,                     KC_H, RSFT(KC_J), RALT(KC_K), RGUI(KC_L), RCTL(KC_SCLN),
+            KC_Z,       KC_X,       KC_C,       KC_V,    KC_B, XXXXXXX, XXXXXXX,   KC_N,       KC_M,    KC_COMM,     KC_DOT,    KC_SLSH,
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, KC_BSPC,  KC_ESC,  KC_ENT, KC_SPC,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX
+    ),
 
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
-  [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
-    KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
-  [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   QK_BOOT, _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, KC_PAUS )
+  [1] = LAYOUT(
+         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6, KC_7,    KC_8,    KC_9,    KC_0,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  XXXXXXX, KC_4,    KC_5,    KC_6, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, KC_1,    KC_2,    KC_3, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, KC_0, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+  [2] = LAYOUT(
+      KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+      KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11,
+      KC_NO,   KC_VOLU, KC_NO,   KC_NO,   QK_BOOT, XXXXXXX, XXXXXXX, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12,
+      KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, KC_PAUS
+    )
 };
